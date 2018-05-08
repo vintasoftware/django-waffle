9c57e88445ecc21e2ccbc9a2d54073cd1b97649e
Hugo Bessa <hugo@vinta.com.br>
2018-05-08 19:29:17 -0300

    Work around mbcs bug in distutils

--- setup.py
+++ setup.py
@@ -1,5 +1,16 @@
 from setuptools import setup, find_packages
 
+# Work around mbcs bug in distutils. 
+# http://bugs.python.org/issue10945
+import codecs     
+try: 
+    codecs.lookup('mbcs') 
+except LookupError: 
+    ascii = codecs.lookup('ascii') 
+    func = lambda name, enc=ascii: {True: enc}.get(name=='mbcs') 
+    codecs.register(func) 
+
+
 setup(
     name='vinta-django-waffle',
     version='0.15.0',
